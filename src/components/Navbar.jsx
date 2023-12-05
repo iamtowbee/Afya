56b8f8008319630df3bdd067eef0e7bf52f8f563
Oluwatobi Oluyede <53963641+iamtowbee@users.noreply.github.com>
2023-12-05 14:42:09 +0000

    Begin hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () =>
 
   return (
     <header className='sticky top-0 z-50 w-full bg-white'>
-      <nav className='container flex items-center py-4 shadow-md md:shadow-sm'>
+      <nav className='container flex items-center shadow-md py-3 md:shadow-sm'>
         <div className="w-32 h-auto px-2 py-1.5 font-bold rounded-md hover:bg-gray-100">
           <Link to="/"><img src={Logo} alt="Afya - Decentralized Medical Data Platform" /></Link>
         </div>
@@ -19,20 +19,22 @@ const Navbar = () =>
           <li className='cursor-pointer hover:text-accent'><a href="#">Login</a></li>
         </ul>
 
-        <button type="button" className='bg-primary text-white flex items-center gap-x-2 px-6 py-4 mr-2 rounded-lg font-medium uppercase hover:bg-secondary hover:shadow-lg'>
+        <button type="button" className='bg-primary text-white hidden md:flex items-center gap-x-2 px-6 py-4 mr-2 rounded-lg font-medium uppercase hover:bg-secondary hover:shadow-lg'>
           <Link to="/" className='text-base font-bold leading-4'>Join Us</Link><ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
         </button>
 
         {/* Hamburger Icon */}
         <div id='menu-btn' className="flex sm:hidden flex-1 justify-end pr-2">
-          <i className="fas fa-bars p-3 rounded-md hover:cursor-pointer hover:bg-primary hover:text-white transition-all relative" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}></i>
+          <ion-icon 
+            name="menu" 
+            className="p-3 rounded-md hover:cursor-pointer hover:bg-primary hover:text-white transition-all relative"
+            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}></ion-icon>
         </div>
         {/* Hamburger Menu */}
         <div id={isHamburgerOpen ? "openHamburger" : "closedHamburger"} className="sm:hidden">
           <div id="ham-menu" className="absolute flex flex-col items-center self-end py-4 mt-9 text-xl font-semibold bg-white sm:w-auto sm:self-center left-12 right-12 drop-shadow-lg transition-all divide-y z-10">
-            <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Resume</a>
-            <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Pricing</a>
-            <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>About</a>
+            <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Home</a>
+            <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Product</a>
             <a href="#" className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Contact</a>
             <Link to='/login' className='text-center w-full py-3 hover:bg-blue-50 text-primary hover:text-accent'>Login</Link>
           </div>
